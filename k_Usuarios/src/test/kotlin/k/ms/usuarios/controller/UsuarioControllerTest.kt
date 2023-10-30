8a92c20fecc201a78918d2dca8fae97289c23685
Josema <sanchezma.dev@gmail.com>
2023-10-30 17:57:33 +0100

    Test para controlador - primer commit

---
+++ k_Usuarios/src/test/kotlin/k/ms/usuarios/controller/UsuarioControllerTest.kt
@@ -0,0 +1,11 @@
+package k.ms.usuarios.controller
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UsuarioControllerTest {
+
+
+}
